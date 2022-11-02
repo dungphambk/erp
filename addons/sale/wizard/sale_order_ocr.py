3bb9c6ffa80c50b83d9a745e9ac64429dde4602b
dungphambk <phammanhdungltl@gmail.com>
2022-11-02 14:52:14 +0700

    update

--- addons/sale/wizard/sale_order_ocr.py
+++ addons/sale/wizard/sale_order_ocr.py
@@ -6,6 +6,7 @@ from odoo import _, api, fields, models
 from odoo.exceptions import UserError
 from odoo.tools import formataddr
 
+from base64 import decodebytes
 from PIL import Image
 import pytesseract
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
@@ -17,8 +18,8 @@ class SaleOrderOcr(models.TransientModel):
     file = fields.Binary(string ="Image")
 
     def action_get_text(self):
-        #result = pytesseract.image_to_string(file)
-        result = pytesseract.image_to_string(Image.open(r'F:\Downloads\ERP\erp\erp\addons\sale\models\test.png'))
+        with open("foo.png","wb") as f:
+            f.write(decodebytes(self.file))
+        result = pytesseract.image_to_string(Image.open(r'F:\Downloads\ERP\erp\erp\foo.png'))
         print(result)
-        return result
-        
+        return
